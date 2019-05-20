612c96c2ca989323007ae3669145fcc7a6cd5175
Zunedi Suprianto. S <42605944+Zunedi@users.noreply.github.com>
2019-05-20 19:45:13 +0700

    Buat Database Post

--- database/migrations/2019_05_20_123947_create_posts_table.php
+++ database/migrations/2019_05_20_123947_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('url');
+            $table->text('content');
+            $table->integer('author_id')->nullable();
             $table->timestamps();
         });
     }
