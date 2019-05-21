b8dd1b06fe1da68280de2a5acb2c39640d8f3f71
adistriani <adistriani@gmail.com>
2019-05-21 11:10:12 +0700

    templating + view user

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b><a href="http://www.fb.com/softsed">Softsed-SimpleAdminLTE V 1.0</a></b> 
-        </div>
-        <strong> <a href="http://almsaeedstudio.com">Almsaeed Studio V 2.3.11</a></strong> 
-      </footer>
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
