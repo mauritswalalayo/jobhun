60bf84c63ca467e687dca4e5453626351ea9784b
mauritswalalayo <48439158+mauritswalalayo@users.noreply.github.com>
2019-05-22 19:03:52 +0700

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,12 +15,12 @@ class UserController extends Controller
         $first_index = $user->currentPage() * $user->perPage() - $user->perPage() + 1;
         return view ('content.user.table', ['daftar_user' => $user, 'first_index' => $first_index]);
     }
-
+//============================================
     public function form ()
     {
         return view ('content.user.form');
     }
-
+//============================================
     public function add (Request $request)
     {
         $validator = validator::make($request->all(),
@@ -36,6 +36,30 @@ class UserController extends Controller
 
         $user = user::create($request->all());
         $user->save();
+        return redirect()->route('user.table');
+    }
+//================================================
+    public function edit ($id)
+    {
+        $user = user::find($id);
+        return view ('content.user.form',['edituser' => $user]);
+    }
+//===============================================
+    public function update (Request $request, $id)
+    {
+        $validator = validator::make($request->all(),
+        ['name' => 'required',
+        'phone' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+        ]);
+        if ($validator->fails())
+        {
+            return back ()-> withErrors($validator)->withInput();
+        }
+
+        $user = user::find($id);
+        $user->update($request->all())->save();
         return redirect()->route('user.table');
     }
 }
