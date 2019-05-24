5ffe119e013c2ea1fdca202fcf8164def4d82c67
Zunedi Suprianto. S <42605944+Zunedi@users.noreply.github.com>
2019-05-24 19:11:46 +0700

    uplaod

--- database/migrations/2019_05_21_112934_create_posts_tags_table.php
+++ database/migrations/2019_05_21_112934_create_posts_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostTagsTable extends Migration
+class CreatePostsTagsTable extends Migration
 {
     /**
      * Run the migrations.
