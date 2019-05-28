68bfc49224dc22701b07e8c54a0b42aba4a8271c
Zunedi Suprianto. S <42605944+Zunedi@users.noreply.github.com>
2019-05-28 19:10:28 +0700

    upload

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,9 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="{{ route('login') }}" method="post">
+      @csrf
+      
       <div class="form-group has-feedback">
         <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
           value="{{ old('email') }}" required autofocus>
