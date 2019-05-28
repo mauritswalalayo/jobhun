02bdf8ec2adb1d807d41424f741045215970bd82
adistriani <adistriani@gmail.com>
2019-05-28 16:15:33 +0700

    hapus tulisan

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,11 +61,6 @@
                             </div>
                         </div>
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
