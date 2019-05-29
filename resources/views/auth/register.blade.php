1e130c7c77b145f0228aebe4fa07d412df59c869
Zunedi Suprianto. S <42605944+Zunedi@users.noreply.github.com>
2019-05-29 15:14:47 +0700

    upload

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,57 +30,51 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href=""><b>Admin</b>LTE</a>
   </div>
 
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="" method="post">
+    <form action="{{ route('register') }}" method="post">
     @csrf
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" id="name" name="name" placeholder="Full name">
+        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus placeholder="Full Name">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
+
+
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
+
+
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
+        <!-- <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone"> -->
+        <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" required placeholder="Phone Number">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
+
+
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
+
+
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" id="retypepassword" name="retypepassword" placeholder="Retype password">
+        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Retype Password">
         <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
+
+
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> I agree to the <a href="#">terms</a>
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
         </div>
-        <!-- /.col -->
       </div>
     </form>
-
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign up using
-        Google+</a>
-    </div>
 
     <a href="{{route('login')}}" class="text-center">I already have a membership</a>
   </div>
