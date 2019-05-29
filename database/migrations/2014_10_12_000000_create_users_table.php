256b1c3dac93cd6a6b73aa527d0d174faa6712a6
Zunedi Suprianto. S <42605944+Zunedi@users.noreply.github.com>
2019-05-29 15:49:04 +0700

    upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('phone');
-            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('role')->nullable();
             $table->string('password')->nullable();
