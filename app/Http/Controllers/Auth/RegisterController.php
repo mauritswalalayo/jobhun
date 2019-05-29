be412e955922883d46e90e60db6294a250f1012f
mauritswalalayo <48439158+mauritswalalayo@users.noreply.github.com>
2019-05-29 20:34:12 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'phone' => $data['phone'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
             
         ]);
     }
