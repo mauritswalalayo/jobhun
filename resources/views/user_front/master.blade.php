29351cba62e4fd64149986309872b82fa5656466
Zunedi <zunedisinambela@gmail.com>
2019-06-19 18:51:43 +0700

    Upload

--- resources/views/user_front/master.blade.php
+++ resources/views/user_front/master.blade.php
@@ -572,14 +572,7 @@
 	</div>
 </div><!-- SIGNUP POPUP -->
 
-<script src="{{asset('user/js/jquery.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/modernizr.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/script.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/bootstrap.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/wow.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/slick.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/parallax.js')}}" type="text/javascript"></script>
-<script src="{{asset('user/js/select-chosen.js')}}" type="text/javascript"></script>
+@include('partial.foot')
 
 </body>
 </html>
