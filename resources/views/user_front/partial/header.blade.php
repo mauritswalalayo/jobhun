612b2a18a1cfbb640fef40260590af96b45fd1bc
Zunedi <zunedisinambela@gmail.com>
2019-06-20 20:03:41 +0700

    mengatur template

---
+++ resources/views/user_front/partial/header.blade.php
@@ -0,0 +1,49 @@
+<div class="container">
+    <div class="logo">
+        <a href="index.html" title=""><img class="hidesticky" src="{{asset('user/images/jobhun1.png')}}" alt="" /><img
+                class="showsticky" src="{{asset('user/images/jobhun1.jpg')}}" alt="" /></a>
+    </div><!-- Logo -->
+    <div class="btn-extars">
+        <a href="#" title="" class="post-job-btn"><i class="la la-plus"></i>Post Jobs</a>
+        <ul class="account-btns">
+            <li class="signup-popup"><a title=""><i class="la la-key"></i> Sign Up</a></li>
+            <li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Login</a>
+            </li>
+        </ul>
+    </div><!-- Btn Extras -->
+    <nav>
+        <ul>
+            <li class="">
+                <a href="#" title="">Tentang Jobhun</a>
+            </li>
+
+            <li class="menu-item-has-children">
+                <a href="#" title="">Layanan</a>
+                <ul>
+                    <li><a href="#"> Jobhun Career Hub</a></li>
+                    <li><a href="#">Jobhun Academy</a></li>
+                    <li><a href="#">Media Partner</a></li>
+                </ul>
+            </li>
+            <li class="menu-item-has-children">
+                <a href="#" title="">Program</a>
+                <ul>
+                    <li><a href="#">Jobhun Internship</a></li>
+                    <li><a href="#">Jobhun Talks</a></li>
+                    <li><a href="#">Jobhun Visit</a></li>
+                </ul>
+            </li>
+            <li class="menu-item-has-children">
+                <a href="#" title="">Blog</a>
+                <ul>
+                    <li><a href="#" title="">Cerita Karier</a></li>
+                    <li><a href="#" title="">Infografik</a></li>
+                    <li><a href="#" title="">Karierpedia</a></li>
+                    <li><a href="#" title="">Artikel</a></li>
+                    <li><a href="#" title="">Berita</a></li>
+                    <li><a href="#" title="">Info Acara</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav><!-- Menus -->
+</div>
