afc50771ed8b017594653b457b07c3f91ecd114a
Zunedi <zunedisinambela@gmail.com>
2019-06-20 20:07:13 +0700

    mengganti template

--- resources/views/user_front/homepage.blade.php
+++ resources/views/user_front/homepage.blade.php
@@ -516,14 +516,7 @@
 		</div>
 	</div><!-- SIGNUP POPUP -->
 
-	<script src="{{asset('user/js/jquery.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/modernizr.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/script.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/bootstrap.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/wow.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/slick.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/parallax.js')}}" type="text/javascript"></script>
-	<script src="{{asset('user/js/select-chosen.js')}}" type="text/javascript"></script>
+	@include('user_front.partial.js')
 
 </body>
 
