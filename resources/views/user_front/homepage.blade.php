4c006a698552880b90cc8646eb3efcde76e5323b
Zunedi <zunedisinambela@gmail.com>
2019-06-20 19:53:54 +0700

    mengatur template

--- resources/views/user_front/homepage.blade.php
+++ resources/views/user_front/homepage.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	@include('user_front.partial.head')
+	@include('user_front.partial.css')
 
 </head>
 
@@ -27,53 +27,8 @@
 					</div>
 				</div>
 			</div>
-			<div class="responsive-opensec">
-				<div class="btn-extars">
-					<a href="#" title="" class="post-job-btn"><i class="la la-plus"></i>Posting Job</a>
-					<ul class="account-btns">
-						<li class="signup-popup"><a title=""><i class="la la-key"></i> Sign Up</a></li>
-						<li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Login</a></li>
-					</ul>
-				</div><!-- Btn Extras -->
-				<form class="res-search">
-					<input type="text" placeholder="Job title, keywords or company name" />
-					<button type="submit"><i class="la la-search"></i></button>
-				</form>
-				<div class="responsivemenu">
-					<ul>
-						<li class="">
-							<a href="#" title="">Tentang Jobhun</a>
-						</li>
-						<li class="menu-item-has-children">
-							<a href="#" title="">Layanan</a>
-							<ul>
-								<li><a href="#"> Jobhun Career Hub</a></li>
-								<li><a href="#">Jobhun Academy</a></li>
-								<li><a href="#">Media Partner</a></li>
-							</ul>
-						</li>
-						<li class="menu-item-has-children">
-							<a href="#" title="">Program</a>
-							<ul>
-								<li><a href="#">Jobhun Internship</a></li>
-								<li><a href="#">Jobhun Talks</a></li>
-								<li><a href="#">Jobhun Visit</a></li>
-							</ul>
-						</li>
-						<li class="menu-item-has-children">
-							<a href="#" title="">Blog</a>
-							<ul>
-								<li><a href="#" title="">Cerita Karier</a></li>
-								<li><a href="#" title="">Infografik</a></li>
-								<li><a href="#" title="">Karierpedia</a></li>
-								<li><a href="#" title="">Artikel</a></li>
-								<li><a href="#" title="">Berita</a></li>
-								<li><a href="#" title="">Info Acara</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
+
+			@include('user_front.partial.header')
 		</div>
 
 		<header class="stick-top forsticky new-header">
