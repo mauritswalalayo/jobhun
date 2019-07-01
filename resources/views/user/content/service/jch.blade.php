b9545116a94ba0e83e966c6ffce9de26ec5ec649
Zunedi <zunedisinambela@gmail.com>
2019-07-01 12:21:10 +0700

    Menambah Foto di Jobhun Career Hub

--- resources/views/user/content/service/jch.blade.php
+++ resources/views/user/content/service/jch.blade.php
@@ -4,8 +4,9 @@
 <section class="overlape">
 	<div class="block no-padding">
 		<div data-velocity="-.1"
-			style="background: url(http://placehold.it/1600x800) repeat scroll 50% 422.28px transparent;"
-			class="parallax scrolly-invisible no-parallax"></div><!-- PARALLAX BACKGROUND IMAGE -->
+			style="background: url({{ asset ('user/images/depan.jpg')}});"
+			class="parallax scrolly-invisible no-parallax">
+		</div><!-- PARALLAX BACKGROUND IMAGE -->
 		<div class="container fluid">
 			<div class="row">
 				<div class="col-lg-12">
