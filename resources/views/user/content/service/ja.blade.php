804f21960485f3273b345e4aa47bb1afbe774b1d
Zunedi <zunedisinambela@gmail.com>
2019-07-01 15:25:20 +0700

    membuat jobhun academy

--- resources/views/user/content/service/ja.blade.php
+++ resources/views/user/content/service/ja.blade.php
@@ -1,5 +1,52 @@
 @extends('user.master')
 
 @section('content')
-    
+<section>
+    <div class="block no-padding">
+        <div data-velocity="-.1" style="background: url({{ asset ('user/images/depan.jpg')}});"
+            class="parallax scrolly-invisible no-parallax">
+        </div><!-- PARALLAX BACKGROUND IMAGE -->
+        <div class="container fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="inner-header">
+                        <h3>Jobhun Academy</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="about-us">
+                        <div class="row">
+                            <div class="col-lg-12">
+                            </div>
+                            <div class="col-lg-7">
+                            </div>
+                            <div class="col-lg-12">
+                                <br><br>
+
+                                <h2 align="center">Mengasah <i>skill</i> bersama Jobhun</h2>
+                                <p align="justify">
+                                    Jobhun Academy merupakan sebuah program pelatihan dengan berbagai topik yang dapat
+                                    menunjang skill para pekerja secara intensif. Melalui program ini, kamu tidak hanya
+                                    belajar secara teori saja, namun juga dilakukan secara praktek sehingga akan
+                                    menghasilkan portfolio terbaik bagi kariermu. Tak hanya itu saja, para mentee dalam
+                                    Jobhun Academy akan dibekali persiapan karier sesuai dengan minatnya. Kamu bisa
+                                    menambah skill di berbagai bidang, meningkatkan karier, mengubah karier, atau
+                                    menjadi seorang entrepreneur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
