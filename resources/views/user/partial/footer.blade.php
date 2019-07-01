32c8a500c171733a7d5f9de04f2561f119cfffdc
Zunedi <zunedisinambela@gmail.com>
2019-07-01 08:53:05 +0700

    Mengganti Partial

---
+++ resources/views/user/partial/footer.blade.php
@@ -0,0 +1,76 @@
+<footer>
+    <div class="block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 column">
+                    <div class="widget">
+                        <div class="about_widget">
+                            <div class="logo">
+                                <a href="{{route('index')}}" title=""><img src="{{ asset('user/images/jobhun.png')}}" height="50" alt="" /></a>
+                            </div>
+                            <span>Semua hal yang dibutuhkan para pencari kerja maupun perusahaan yang sedang
+                                mencari pekerja ada di Jobhun. Pencari kerja dapat menemukan pekerjaan,
+                                sekaligus mengembangkan kemampuannya. Sedangkan perusahaan bisa mencari pekerja
+                                dengan cara yang cepat dan mudah.</span>
+                        </div><!-- About Widget -->
+                    </div>
+                </div>
+                <div class="col-md-3 column">
+                    <div class="widget">
+                        <h3 class="footer-title">Jobhun</h3>
+                        <div class="link_widgets">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <a href="#" title="">Kerja Sama Media Partner</a>
+                                    <a href="#" title="">Syarat dan Ketentuan</a>
+                                    <a href="#" title="">Kebijakan Privasi</a>
+                                    <a href="#" title="">Karier di Jobhun</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 column">
+                    <div class="widget">
+                        <h3 class="footer-title">Hubungi Kami</h3>
+                        <div class="about_widget">
+                            <span>Jobhun – CV Sindikat Kreasi Digital.</span>
+                            <hr>
+                            <span>Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel, Wonokromo,
+                                Surabaya, Jawa Timur, Indonesia 60246.</span>
+                            <span>E-mail: info@jobhun.id</span>
+                            <span>Telp. & WA: 08113235533.</span>
+                        </div><!-- About Widget -->
+                    </div>
+                </div>
+                <div class="col-md-3 column">
+                    <div class="widget">
+                        <h3 class="footer-title">Media Sosial Jobhun</h3>
+                        <div class="about_widget">
+                            <div class="social">
+                                <a href="https://www.youtube.com/channel/UCCzKrMp711xzX8nI8zempKw?view_as=subscriber" target="_blank" title=""><i class="fa fa-youtube"></i></a>
+                                <a href="https://www.instagram.com/jobhun/" target="_blank" title=""><i class="fa fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/company/jobhun-surabaya/" target="_blank" title=""><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div><!-- About Widget -->
+                        <div class="about_widget">
+                            <h3 class="footer-title">Dapatkan update terbaru dari Jobhun!</h3>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="col-lg-3 column">
+                    <div class="widget">
+                        <div class="download_widget">
+                            <a href="#" title=""><img src="http://placehold.it/230x65" alt="" /></a>
+                            <a href="#" title=""><img src="http://placehold.it/230x65" alt="" /></a>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+    <div class="bottom-line">
+        <span>© 2018 Jobhunt All rights reserved. Design by Creative Layers</span>
+        <a href="#scrollup" class="scrollup" title=""><i class="la la-arrow-up"></i></a>
+    </div>
+</footer>
