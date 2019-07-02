28c3e305a575ee32f9dc6c889a47c4842d52d954
Zunedi <zunedisinambela@gmail.com>
2019-07-02 16:23:00 +0700

    jobhun academy

--- resources/views/user/content/service/ja.blade.php
+++ resources/views/user/content/service/ja.blade.php
@@ -30,7 +30,6 @@
                             </div>
                             <div class="col-lg-12">
                                 <br><br>
-
                                 <h2 align="center">Mengasah <i>skill</i> bersama Jobhun</h2>
                                 <p align="justify">
                                     Jobhun Academy merupakan sebuah program pelatihan dengan berbagai topik yang dapat
@@ -50,7 +49,10 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Belajar dengan formula khusus untuk persiapan karier</h3>
-                                            <p align="left">Di Jobhun Academy, kamu tidak hanya belajar secara teori saja. Namun juga akan praktek secara langsung untuk menghasilkan portfolio penunjang karier. Kamu juga bisa sharing bersama mentor untuk membahas persiapan karier.</p>
+                                            <p align="left">Di Jobhun Academy, kamu tidak hanya belajar secara teori
+                                                saja. Namun juga akan praktek secara langsung untuk menghasilkan
+                                                portfolio penunjang karier. Kamu juga bisa sharing bersama mentor untuk
+                                                membahas persiapan karier.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -58,7 +60,11 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Berkesempatan untuk mendapatkan pekerjaan</h3>
-                                            <p align="left">Jobhun bekerjasama dengan berbagai perusahaan dalam mencari kandidat terbaik. Apabila kamu berhasil menyelesaikan program di Jobhun Academy dengan baik dan sesuai dengan Kriteria yang sedang dicari oleh perusahaan, maka Jobhun akan merekomendasikan kamu agar bisa bekerja di beberapa company partner Jobhun tersebut.</p>
+                                            <p align="left">Jobhun bekerjasama dengan berbagai perusahaan dalam mencari
+                                                kandidat terbaik. Apabila kamu berhasil menyelesaikan program di Jobhun
+                                                Academy dengan baik dan sesuai dengan Kriteria yang sedang dicari oleh
+                                                perusahaan, maka Jobhun akan merekomendasikan kamu agar bisa bekerja di
+                                                beberapa company partner Jobhun tersebut.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -66,7 +72,9 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Dibimbing oleh mentor - mentor berkualitas dan berpengalaman</h3>
-                                            <p align="left">Jobhun selalu berupaya menghadirkan mentor berkualitas dan berpengalaman dibidangnya, serta memiliki prestasi di bidang tersebut.</p>
+                                            <p align="left">Jobhun selalu berupaya menghadirkan mentor berkualitas dan
+                                                berpengalaman dibidangnya, serta memiliki prestasi di bidang tersebut.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -77,7 +85,10 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Belajar secara optimal dan kondusif</h3>
-                                            <p align="left">Agar belajar bisa lebih optimal dan kondusif, Jobhun Academy hanya bisa diikuti 3 - 5 orang setiap kelas. Kami percaya bahwa kapasitas kelas yang sedikit akan membuat interaksi antara mentor dan mentee menjadi lebih intens.</p>
+                                            <p align="left">Agar belajar bisa lebih optimal dan kondusif, Jobhun Academy
+                                                hanya bisa diikuti 3 - 5 orang setiap kelas. Kami percaya bahwa
+                                                kapasitas kelas yang sedikit akan membuat interaksi antara mentor dan
+                                                mentee menjadi lebih intens.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -85,7 +96,10 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Menghasilkan portfolio yang berguna untuk karier</h3>
-                                            <p align="left">Setelah program Jobhun Academy ini berakhir, mentee akan mendapatkan hasil tes, penilaian secara keseluruhan, sertifikat, dan portfolio atau karya di masing - masing bidang yang telah dilakukan selama pembelajaran dilakukan.</p>
+                                            <p align="left">Setelah program Jobhun Academy ini berakhir, mentee akan
+                                                mendapatkan hasil tes, penilaian secara keseluruhan, sertifikat, dan
+                                                portfolio atau karya di masing - masing bidang yang telah dilakukan
+                                                selama pembelajaran dilakukan.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -93,13 +107,31 @@
                                     <div class="job-grid border">
                                         <div class="job-title-sec">
                                             <h3>Biaya terjangkau dan bisa dicicil</h3>
-                                            <p align="left">Kamu tidak perlu pusing masalah biaya. Program - program yang ada di Jobhun Academy dipatok dengan harga yang terjangkau dan bisa kamu cicil dengan skema cicilan yang sangat membantu.</p>
+                                            <p align="left">Kamu tidak perlu pusing masalah biaya. Program - program
+                                                yang ada di Jobhun Academy dipatok dengan harga yang terjangkau dan bisa
+                                                kamu cicil dengan skema cicilan yang sangat membantu.</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="block ">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="how-works">
+                        <div class="row"><img src="{{asset ('user/images/ja.jpeg')}}" alt="" /></div>
+                    </div>
+                    <h2 align="center">Pilihan program</h2>
+                    <p align="center">Mana karier yang kamu pilih? Pilih program di Jobhun Academy yang sesuai dengan
+                        tujuanmu! (Klik pada gambar)</p>
                 </div>
             </div>
         </div>
