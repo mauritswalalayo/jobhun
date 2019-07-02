3ba1d817278ed3ba2916140a67201f240037c47d
Zunedi <zunedisinambela@gmail.com>
2019-07-02 17:23:01 +0700

    halaman depan dan spesifikasi jobhun academy

--- resources/views/user/content/service/spek_ja.blade.php
+++ resources/views/user/content/service/spek_ja.blade.php
@@ -67,37 +67,90 @@
                                     bisa mempersiapkan karier sebagai seorang content writer melalui program ini.
                                 </p>
                                 <p>Mengapa kamu harus mengikuti Jobhun Academy: Content Writer ini? <br>
-                                    <ol type="1">
-                                        Belajar dengan formula khusus untuk persiapan karier
-                                    </ol>
-                                    1. Belajar dengan formula khusus untuk persiapan karier Di Jobhun Academy, kamu
+                                    1. Belajar dengan formula khusus untuk persiapan karier <br>
+                                    Di Jobhun Academy, kamu
                                     tidak hanya belajar secara teori saja. Namun juga akan praktek secara langsung untuk
                                     menghasilkan portfolio penunjang karier. Kamu juga bisa sharing bersama mentor untuk
                                     membahas persiapan karier. <br>
-                                    2. Berkesempatan untuk mendapatkan pekerjaan Jobhun bekerjasama dengan berbagai
-                                    perusahaan dalam mencari kandidat terbaik. Apabila kamu berhasil menyelesaikan
-                                    program di Jobhun Academy dengan baik dan sesuai dengan kriteria yang sedang dicari
-                                    oleh perusahaan, maka Jobhun akan merekomendasikan kamu agar bisa bekerja di
-                                    beberapa company partner Jobhun tersebut. <br>
-                                    3. Dibimbing oleh mentor-mentor berkualitas dan berpengalaman Jobhun selalu berupaya
-                                    menghadirkan mentor berkualitas dan berpengalaman dibidangnya, serta memiliki
-                                    prestasi di bidang tersebut. <br>
-                                    4. Belajar secara optimal dan kondusif Agar belajar bisa lebih optimal dan kondusif,
+                                    2. Berkesempatan untuk mendapatkan pekerjaan <br>
+                                    Jobhun bekerjasama dengan berbagai perusahaan dalam mencari kandidat terbaik.
+                                    Apabila kamu berhasil menyelesaikan program di Jobhun Academy dengan baik dan sesuai
+                                    dengan kriteria yang sedang dicari oleh perusahaan, maka Jobhun akan
+                                    merekomendasikan kamu agar bisa bekerja di beberapa company partner Jobhun tersebut.
+                                    <br>
+                                    3. Dibimbing oleh mentor-mentor berkualitas dan berpengalaman <br>
+                                    Jobhun selalu berupaya menghadirkan mentor berkualitas dan berpengalaman
+                                    dibidangnya, serta memiliki prestasi di bidang tersebut. <br>
+                                    4. Belajar secara optimal dan kondusif Agar belajar bisa lebih optimal dan kondusif
+                                    <br>
                                     Jobhun Academy hanya bisa diikuti 3-5 orang setiap kelas. Kami percaya bahwa
                                     kapasitas kelas yang sedikit akan membuat interaksi antara mentor dan mentee menjadi
                                     lebih intens. <br>
-                                    5. Menghasilkan portfolio yang berguna untuk karier Setelah program Jobhun Academy
-                                    ini berakhir, mentee akan mendapatkan hasil tes, penilaian secara keseluruhan,
-                                    sertifikat, dan portfolio atau karya di masing-masing bidang yang telah dilakukan
-                                    selama pembelajaran dilakukan. <br>
-                                    6. Biaya terjangkau dan bisa dicicil Kamu tidak perlu pusing masalah biaya.
-                                    Program-program yang ada di Jobhun Academy dipatok dengan harga yang terjangkau dan
-                                    bisa kamu cicil dengan skema cicilan yang
+                                    5. Menghasilkan portfolio yang berguna untuk karier <br> Setelah program Jobhun
+                                    Academy ini berakhir, mentee akan mendapatkan hasil tes, penilaian secara
+                                    keseluruhan, sertifikat, dan portfolio atau karya di masing-masing bidang yang telah
+                                    dilakukan selama pembelajaran dilakukan. <br>
+                                    6. Biaya terjangkau dan bisa dicicil <br>
+                                    Kamu tidak perlu pusing masalah biaya. Program-program yang ada di Jobhun Academy
+                                    dipatok dengan harga yang terjangkau dan bisa kamu cicil dengan skema cicilan yang
                                     sangat membantu.
                                 </p>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 column">
+                    <div class="job-single-sec">
+                        <div class="job-details">
+                            <h3>Job Description</h3>
+                            <p>
+                                Tuupai adalah cara baru dan praktis untuk mencari segala macam penyedia jasa profesional
+                                di sekitar Anda. Tuupai berbasismobile service marketplace yang mana secara langsung
+                                bisa menghubungkan Anda dengan berbagai penyedia jasa profesional tanpa melalui
+                                perantara. Saat ini Tuupai sedang membuka program magang untuk beberapa posisi:
+                            </p>
+                            <p>
+                                <ul>
+                                    <li>Account Executive</li>
+                                    <li>Copywriter</li>
+                                    <li>Client Relation</li>
+                                    <li>Fotografer Indoor</li>
+                                    <li>Mobile Developer</li>
+                                    <li>UI Designer</li>
+                                    <li>Ilustrator</li>
+                                    <li>Videographer</li>
+                                </ul>
+                            </p>
+                            <p>Cek bit.ly/tuupailab untuk cek kualifikasi dan pendaftaran.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 column">
+                    <div class="job-single-head style2">
+                        <div class="job-thumb"> <img src="{{ asset ('user/images/job1.jpg')}}" width="124" height="124"
+                                alt="" /> </div>
+                        <div class="job-head-info">
+                            <h4>Magang di Tuupai</h4>
+                            <span>#Duduk Santai Panggil Tuupai</span>
+                            <p><i class="la la-unlink"></i>https://www.tuupai.com/</p>
+                            <p><i class="la la-phone"></i> +90 538 963 54 87</p>
+                            <p><i class="la la-envelope-o"></i> ali.tufan@jobhunt.com</p>
+                        </div>
+                        <a href="#" title="" class="apply-job-btn"><i class="la la-paper-plane"></i>Apply for
+                            job</a>
+                        <a href="#" title="" class="apply-job-linkedin"><i class="la la-linkedin"></i>Apply with
+                            Linkedin</a>
+                        <a href="{{ route ('jch')}}" title="" class="viewall-jobs">View all Jobs</a>
+                    </div><!-- Job Head -->
                 </div>
             </div>
         </div>
