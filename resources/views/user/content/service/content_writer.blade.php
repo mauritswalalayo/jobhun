271de721863fc559d8e3db2f23138869444a7474
Zunedi <zunedisinambela@gmail.com>
2019-07-03 15:00:08 +0700

    content writer

--- resources/views/user/content/service/content_writer.blade.php
+++ resources/views/user/content/service/content_writer.blade.php
@@ -99,34 +99,103 @@
                                         <h3>Materi yang akan kamu pelajari</h3>
                                         <p>
                                             <ul>
-                                                <li>Mengenal content writing dan mengapa penting untuk mempelajarinya</li>
-                                                <li>Mengetahui apa saja tugas dan tanggung jawab seorang content writer. Apa bedanya dengan profesi lain yang berkaitan dengan kepenulisan.</li>
-                                                <li>Mengetahui urutan jabatan untuk berkarier sebagai content writer</li>
-                                                <li>Mengetahui prospek kerja dan peluang-peluang bekerja sebagai content writer</li>
-                                                <li>Mengetahui pengetahuan dan keterampilan apa saja yang harus dimiliki oleh seorang content writer</li>
-                                                <li>Mengetahui keterkaitan antara content writing dengan kebutuhan promosi komersial pada sebuah merek. Apa yang membedakan dengan content writing untuk kebutuhan bukan untuk komersial.</li>
+                                                <li>Mengenal content writing dan mengapa penting untuk mempelajarinya
+                                                </li>
+                                                <li>Mengetahui apa saja tugas dan tanggung jawab seorang content writer.
+                                                    Apa bedanya dengan profesi lain yang berkaitan dengan kepenulisan.
+                                                </li>
+                                                <li>Mengetahui urutan jabatan untuk berkarier sebagai content writer
+                                                </li>
+                                                <li>Mengetahui prospek kerja dan peluang-peluang bekerja sebagai content
+                                                    writer</li>
+                                                <li>Mengetahui pengetahuan dan keterampilan apa saja yang harus dimiliki
+                                                    oleh seorang content writer</li>
+                                                <li>Mengetahui keterkaitan antara content writing dengan kebutuhan
+                                                    promosi komersial pada sebuah merek. Apa yang membedakan dengan
+                                                    content writing untuk kebutuhan bukan untuk komersial.</li>
                                                 <li>Starter kit for content writer</li>
                                                 <li>Essential for skillsets for content writer</li>
-                                                <li>Mengetahui cara menulis konten yang sesuai dengan target audience</li>
+                                                <li>Mengetahui cara menulis konten yang sesuai dengan target audience
+                                                </li>
                                                 <li>Mengetahui cara menulis konten yang dapat menaikkan trafik</li>
                                                 <li>Mengetahui cara mencari dan menentukan topik yang tepat</li>
-                                                <li>Mengetahui cara mencari topik-topik yang sedang tren dibicarakan menggunakan tools</li>
+                                                <li>Mengetahui cara mencari topik-topik yang sedang tren dibicarakan
+                                                    menggunakan tools</li>
                                                 <li>Membuat struktur-struktur yang ada pada konten sebelum ditulis</li>
-                                                <li>Menulis judul yang sesuai dengan isi tulisan serta mampu menarik pembaca</li>
+                                                <li>Menulis judul yang sesuai dengan isi tulisan serta mampu menarik
+                                                    pembaca</li>
                                                 <li>Membuat struktur penulisan dari awal sampai akhir</li>
                                                 <li>Menulis konten yang sesuai dengan kaidah SEO</li>
-                                                <li>Menulis dengan kaidah penulisan EYD yang baik dan benar. Menulis kalimat yang strukturnya sesuai dan enak dibaca.</li>
+                                                <li>Menulis dengan kaidah penulisan EYD yang baik dan benar. Menulis
+                                                    kalimat yang strukturnya sesuai dan enak dibaca.</li>
                                                 <li>Mengetahui cara meminimalisir typo dalam penulisan</li>
-                                                <li>Menggunakan WordPress atau CMS lainnya untuk keperluan posting konten secara <i>basic</i></li>
-                                                <li>Mengecek performa konten, berdasarkan KPI dan Basic Google Analytics.</li>
-                                                <li>Mengetahui cara melakukan pengecekan orisinalitas tulisan menggunakan <i>software</i></li>
-                                                <li>Mengetahui hal-hal apa saja yang biasanya diperhatikan oleh editor saat memeriksa konten. Bagian mana saja yang biasanya sering memerlukan revisi.</li>
-                                                <li>Mengetahui kiat-kiat memilih gambar yang berkualitas dan tepat untuk konten</li>
-                                                <li>Mengetahui trik membuat tulisan yang bisa menembus editor, kemudian dimuat di media online berkualitas.</li>
+                                                <li>Menggunakan WordPress atau CMS lainnya untuk keperluan posting
+                                                    konten secara <i>basic</i></li>
+                                                <li>Mengecek performa konten, berdasarkan KPI dan Basic Google
+                                                    Analytics.</li>
+                                                <li>Mengetahui cara melakukan pengecekan orisinalitas tulisan
+                                                    menggunakan <i>software</i></li>
+                                                <li>Mengetahui hal-hal apa saja yang biasanya diperhatikan oleh editor
+                                                    saat memeriksa konten. Bagian mana saja yang biasanya sering
+                                                    memerlukan revisi.</li>
+                                                <li>Mengetahui kiat-kiat memilih gambar yang berkualitas dan tepat untuk
+                                                    konten</li>
+                                                <li>Mengetahui trik membuat tulisan yang bisa menembus editor, kemudian
+                                                    dimuat di media online berkualitas.</li>
                                                 <li>Mengetahui perbedaan tulisan di media cetak dan media online</li>
-                                                <li>Mengetahui cara mencari uang melalui tulisan, baik melalui pekerjaan tetap maupun freelance.</li>
-                                                <li>Mengetahui tips mencari pekerjaan sebagai content writer, baik di media maupun perusahaan non media. Mulai dari mempersiapkan CV dan portfolio, interview, tes menulis, hingga diterima.</li>
-                                                <li>Tes akhir untuk mentee (Hasil tes berupa konten yang bisa dijadikan sebagai portfolio dalam melamar pekerjaan)</li>
+                                                <li>Mengetahui cara mencari uang melalui tulisan, baik melalui pekerjaan
+                                                    tetap maupun freelance.</li>
+                                                <li>Mengetahui tips mencari pekerjaan sebagai content writer, baik di
+                                                    media maupun perusahaan non media. Mulai dari mempersiapkan CV dan
+                                                    portfolio, interview, tes menulis, hingga diterima.</li>
+                                                <li>Tes akhir untuk mentee (Hasil tes berupa konten yang bisa dijadikan
+                                                    sebagai portfolio dalam melamar pekerjaan)</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8 column">
+                                <div class="job-single-sec">
+                                    <div class="job-details">
+                                        <h3>Waktu Pelaksanaan</h3>
+                                        <p>Total 8 kali pertemuan (Jadwal pelaksanaan akan disesuaikan dengan
+                                            kesepakatan antara mentor dan seluruh peserta yang nantinya sudah mendaftar)
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8 column">
+                                <div class="job-single-sec">
+                                    <div class="job-details">
+                                        <h3>Tempat Pelaksanaan</h3>
+                                        <p>Jobhun Office, Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel,
+                                            Wonokromo, Surabaya 60246</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 column">
+                                <div class="job-single-sec">
+                                    <div class="job-details">
+                                        <h3>Mentor yang akan mengajar</h3>
+                                        <p>Stella Azasya (Creative Writer at IDN Times)</p>
+                                    </div>
+                                    <div class="how-works">
+                                        <div class="row"><img src="{{asset ('user/images/mentorcw.jpeg')}}"
+                                                alt="Gambar Content Writer" /></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-8 column">
+                                <div class="job-single-sec">
+                                    <div class="job-details">
+                                        <h3>Apa yang wajib dibawa saat Jobhun Academy berlangsung ?</h3>
+                                        <p>
+                                            <ul>
+                                                <li>Laptop</li>
+                                                <li>Alat tulis</li>
+                                                <li>MS Office yang sudag ter-install di laptop</li>
                                             </ul>
                                         </p>
                                     </div>
