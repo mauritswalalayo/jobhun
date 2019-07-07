e25939865a77fccce252f2df7947dbbba6adb956
Zunedi <zunedisinambela@gmail.com>
2019-07-08 00:11:42 +0700

    Jobhun academy

--- resources/views/user/content/service/android2.blade.php
+++ resources/views/user/content/service/android2.blade.php
@@ -16,151 +16,156 @@
 							<div class="job-thumb"> <img src="{{ asset ('user/images/android.jpeg')}}" alt="" /> </div>
 						</div><!-- Job Head -->
 						<div class="job-details">
-							<h3>Job Description</h3>
-							<p>Company is a 2016 Iowa City-born start-up that develops consectetuer adipiscing elit.
-								Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel,
-								dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor
-								libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo.
-								Suspendisse potenti.</p>
-							<p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero
-								eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed
-								lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum
-								volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien</p>
-							<h3>Required Knowledge, Skills, and Abilities</h3>
+							<h3>Deskripsi</h3>
+							<p align="justify">Tidak berbeda jauh dengan web developer, Android developer juga menjadi
+								salah satu profesi yang banyak dicari. Menjamurnya digital startup di kalangan pebisnis
+								membuat banyak perusahaan ingin turut serta mendigitalisasi bisnisnya. Banyak perusahaan
+								berlomba-lomba ingin menciptakan sebuah produk melalui aplikasi Android. Di sini, peran
+								seorang Android developer sangat besar dalam membangun produk sebuah perusahaan. Tidak
+								hanya perusahaan saja yang membutuhkan profesi ini, namun seorang Android Developer juga
+								bisa bekerja secara mandiri. Profesi yang satu ini juga bisa dijalani dengan sistem
+								kerja full time, part time, remote, hingga freelance.</p>
+
+							<p align="justify">Prospek karier untuk profesi ini tentu sangat cerah, apalagi seorang
+								Android developer terkadang tidak harus terikat satu perusahaan saja, sehingga
+								kesempatan untuk mendapatkan penghasilan yang lebih banyak akan terbuka lebar. Seorang
+								Android developer harus memiliki beberapa kemampuan agar siap bersaing di industri. Di
+								Jobhun Academy, kamu bisa belajar menjadi seorang Android developer bersama dengan
+								pakarnya langsung. Tidak hanya belajar secara teori saja, namun kamu juga akan praktek,
+								sehingga bisa menghasilkan portfolio yang akan digunakan untuk mencari kerjaan nantinya.
+								Selain itu, kamu juga bisa mempersiapkan karier sebagai seorang Android developer
+								melalui program ini.</p>
+
+							<h3>Mengapa kamu harus mengikuti Jobhun Academy: Android Developer ini ?</h3>
+							<p>
+								1. Belajar dengan formula khusus untuk persiapan karier <br>
+
+								Di Jobhun Academy, kamu tidak hanya belajar secara teori saja. Namun juga akan praktek
+								secara langsung untuk menghasilkan portfolio penunjang karier. Kamu juga bisa sharing
+								bersama mentor untuk membahas persiapan karier. <br>
+
+								2. Berkesempatan untuk mendapatkan pekerjaan <br>
+
+								Jobhun bekerjasama dengan berbagai perusahaan dalam mencari kandidat terbaik. Apabila
+								kamu berhasil menyelesaikan program di Jobhun Academy dengan baik dan sesuai dengan
+								kriteria yang sedang dicari oleh perusahaan, maka Jobhun akan merekomendasikan kamu agar
+								bisa bekerja di beberapa company partner Jobhun tersebut. <br>
+
+								3. Dibimbing oleh mentor-mentor berkualitas dan berpengalaman <br>
+
+								Jobhun selalu berupaya menghadirkan mentor berkualitas dan berpengalaman dibidangnya,
+								serta memiliki prestasi di bidang tersebut. <br>
+
+								4. Belajar secara optimal dan kondusif <br>
+
+								Agar belajar bisa lebih optimal dan kondusif, Jobhun Academy hanya bisa diikuti 3-5
+								orang setiap kelas. Kami percaya bahwa kapasitas kelas yang sedikit akan membuat
+								interaksi antara mentor dan mentee menjadi lebih intens. <br>
+
+								5. Menghasilkan portfolio yang berguna untuk karier <br>
+
+								Setelah program Jobhun Academy ini berakhir, mentee akan mendapatkan hasil tes,
+								penilaian secara keseluruhan, sertifikat, dan portfolio atau karya di masing-masing
+								bidang yang telah dilakukan selama pembelajaran dilakukan. <br>
+
+								6. Biaya terjangkau dan bisa dicicil <br>
+
+								Kamu tidak perlu pusing masalah biaya. Program-program yang ada di Jobhun Academy
+								dipatok dengan harga yang terjangkau dan bisa kamu cicil dengan skema cicilan yang
+								sangat membantu. <br>
+							</p>
+							<h3>Materi yang akan kamu pelajari :</h3>
 							<ul>
-								<li>Ability to write code – HTML & CSS (SCSS flavor of SASS preferred when writing CSS)
+								<li>Instalasi Intellij Idea</li>
+								<li>Dasar-dasar pemrograman Java, variabel, tipe data, if else, switch case, for loop
 								</li>
-								<li>Proficient in Photoshop, Illustrator, bonus points for familiarity with Sketch
-									(Sketch is our preferred concepting)</li>
-								<li>Cross-browser and platform testing as standard practice</li>
-								<li>Experience using Invision a plus</li>
-								<li>Experience in video production a plus or, at a minimum, a willingness to learn</li>
-							</ul>
-							<h3>Education + Experience</h3>
-							<ul>
-								<li>Advanced degree or equivalent experience in graphic and web design</li>
-								<li>3 or more years of professional design experience</li>
-								<li>Direct response email experience</li>
-								<li>Ecommerce website design experience</li>
-								<li>Familiarity with mobile and web apps preferred</li>
-								<li>Excellent communication skills, most notably a demonstrated ability to solicit and
-									address creative and design feedback</li>
-								<li>Must be able to work under pressure and meet deadlines while maintaining a positive
-									attitude and providing exemplary customer service</li>
-								<li>Ability to work independently and to carry out assignments to completion within
-									parameters of instructions given, prescribed routines, and standard accepted
-									practices</li>
+								<li>Latihan dasar-dasar perintah sederhana</li>
+								<li>Pengenalan Android Studio</li>
+								<li>Dasar-dasar pemrograman Java pada Android</li>
+								<li>Belajar membuat layout Aplikasi dengan XML</li>
+								<li>Pengenalan Activity</li>
+								<li>Pengenalan Intent</li>
+								<li>Menggunakan listview dan recycleview</li>
+								<li>Pengenalan database SQLite</li>
+								<li>Membuat aplikasi menggunakan SQLite</li>
+								<li>Pengenalan SharedPreference</li>
+								<li>Dasar-dasar background service</li>
+								<li>Membuat aplikasi Alarm</li>
+								<li>Persiapan publish aplikasi ke Playstore</li>
 							</ul>
 						</div>
 						<div class="share-bar">
-							<span>Share</span><a href="#" title="" class="share-fb"><i class="fa fa-facebook"></i></a><a
-								href="#" title="" class="share-twitter"><i class="fa fa-twitter"></i></a>
-						</div>
-						<div class="recent-jobs">
-							<h3>Recent Jobs</h3>
-							<div class="job-list-modern">
-								<div class="job-listings-sec no-border">
-									<div class="job-listing wtabs">
-										<div class="job-title-sec">
-											<div class="c-logo"> <img src="http://placehold.it/98x51" alt="" /> </div>
-											<h3><a href="#" title="">Web Designer / Developer</a></h3>
-											<span>Massimo Artemisis</span>
-											<div class="job-lctn"><i class="la la-map-marker"></i>Sacramento, California
-											</div>
-										</div>
-										<div class="job-style-bx">
-											<span class="job-is ft">Full time</span>
-											<span class="fav-job"><i class="la la-heart-o"></i></span>
-											<i>5 months ago</i>
-										</div>
-									</div>
-									<div class="job-listing wtabs">
-										<div class="job-title-sec">
-											<div class="c-logo"> <img src="http://placehold.it/98x51" alt="" /> </div>
-											<h3><a href="#" title="">C Developer (Senior) C .Net</a></h3>
-											<span>Massimo Artemisis</span>
-											<div class="job-lctn"><i class="la la-map-marker"></i>Sacramento, California
-											</div>
-										</div>
-										<div class="job-style-bx">
-											<span class="job-is pt ">Part time</span>
-											<span class="fav-job"><i class="la la-heart-o"></i></span>
-											<i>5 months ago</i>
-										</div>
-									</div><!-- Job -->
-									<div class="job-listing wtabs">
-										<div class="job-title-sec">
-											<div class="c-logo"> <img src="http://placehold.it/98x51" alt="" /> </div>
-											<h3><a href="#" title="">Regional Sales Manager South</a></h3>
-											<span>Massimo Artemisis</span>
-											<div class="job-lctn"><i class="la la-map-marker"></i>Sacramento, California
-											</div>
-										</div>
-										<div class="job-style-bx">
-											<span class="job-is ft ">Full time</span>
-											<span class="fav-job"><i class="la la-heart-o"></i></span>
-											<i>5 months ago</i>
-										</div>
-									</div><!-- Job -->
-									<div class="job-listing wtabs">
-										<div class="job-title-sec">
-											<div class="c-logo"> <img src="http://placehold.it/98x51" alt="" /> </div>
-											<h3><a href="#" title="">Marketing Dairector</a></h3>
-											<span>Massimo Artemisis</span>
-											<div class="job-lctn"><i class="la la-map-marker"></i>Sacramento, California
-											</div>
-										</div>
-										<div class="job-style-bx">
-											<span class="job-is ft ">Full time</span>
-											<span class="fav-job"><i class="la la-heart-o"></i></span>
-											<i>5 months ago</i>
-										</div>
-									</div><!-- Job -->
-								</div>
-							</div>
+							<span>Share</span><a href="https://id-id.facebook.com/login/" target="_blank" title=""
+								class="share-fb"><i class="fa fa-facebook"></i></a><a
+								href="https://twitter.com/login?lang=id" target="_blank" title=""
+								class="share-twitter"><i class="fa fa-twitter"></i></a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-4 column">
-					<a class="apply-thisjob" href="#" title=""><i class="la la-paper-plane"></i>Apply for job</a>
-					<div class="apply-alternative">
-						<a href="#" title=""><i class="la la-linkedin"></i> Apply with Linkedin</a>
-						<span><i class="la la-heart-o"></i> Shortlist</span>
-					</div>
+					<a class="apply-thisjob" href="#" title=""><i class="la la-paper-plane"></i>Registrasi</a>
 					<div class="job-overview">
-						<h3>Job Overview</h3>
+						<h3>Keterangan : </h3>
 						<ul>
 							<li><i class="la la-money"></i>
-								<h3>Offerd Salary</h3><span>£15,000 - £20,000</span>
+								<h3>Rp. 1.500.000,- (Apabila dibayar lunas)</h3>
+								<span>Bisa dibayar lunas atau dicicil. Jika dicicil, menggunakan skema cicilan sebagai
+									berikut : <br>
+									1. DP Rp750.000,- (Dibayar sebelum kelas dimulai) <br>
+									2. Pembayaran ke-1 Rp280.000,- (Dibayar saat pertemuan kedua) <br>
+									3. Pembayaran ke-2 Rp280.000,- (Dibayar saat pertemuan keempat) <br>
+									4. Pembayaran ke-3 Rp280.000,-. (Dibayar saat pertemuan keenam)</span>
 							</li>
-							<li><i class="la la-mars-double"></i>
-								<h3>Gender</h3><span>Female</span>
+							<li><i class="la la-clock-o"></i>
+								<h3>Waktu Pelaksanaan</h3>
+								<span>Total 8 kali pertemuan (Jadwal pelaksanaan akan disesuaikan dengan kesepakatan
+									antara mentor dan seluruh peserta yang nantinya sudah mendaftar)</span>
 							</li>
-							<li><i class="la la-thumb-tack"></i>
-								<h3>Career Level</h3><span>Executive</span>
+							<li><i class="la la-map-marker"></i>
+								<h3>Tempat Pelaksanaan</h3>
+								<span>Jobhun Office, Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel,
+									Wonokromo, Surabaya 60246
+								</span>
 							</li>
-							<li><i class="la la-puzzle-piece"></i>
-								<h3>Industry</h3><span>Management</span>
+							<li><i class="la la-graduation-cap"></i>
+								<h3>Apa yang wajib dibawa saat Jobhun Academy berlangsung ?</h3>
+								<span>
+									1. Laptop <br>
+									2. Alat tulis <br>
+									3. MS Office yang sudag ter-install di laptop
+								</span>
 							</li>
-							<li><i class="la la-shield"></i>
-								<h3>Experience</h3><span>2 Years</span>
-							</li>
-							<li><i class="la la-line-chart "></i>
-								<h3>Qualification</h3><span>Bachelor Degree</span>
+							<li><i class="la la-briefcase"></i>
+								<h3>Fasilitas</h3>
+								<span>
+									1. Makan setiap pertemuan <br>
+									2. Materi dan modul <br>
+									3. Certificate of Completion <br>
+									4. Form penilaian yang menunjukkan perkembangan skill mentee <br>
+									5. Konsultasi dengan mentor <br>
+									6. Ruangan ber-AC, LCD & proyektor, papan tulis, dan Wi-Fi
+								</span>
 							</li>
 						</ul>
 					</div><!-- Job Overview -->
 					<div class="job-location">
-						<h3>Job Location</h3>
-						<div class="job-lctn-map">
-							<iframe
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d926917.0482572999!2d-104.57738594649922!3d40.26036964524562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2s!4v1513784737244"></iframe>
+						<h3>Mentor yang akan mengajar</h3>
+						<div class="col-lg-12 column">
+							<div class="blog-single">
+								<div class="bs-thumb">
+									<img src="{{ asset ('user/images/mentorandroid.jpg')}}"
+										alt="Gambar Web Developer" />
+									<div class="extra-job-info">
+										<span><strong>Muhammad Irsyad</strong> <br>( Trainer for Android & Co-founder at
+											Lazday Indonesia )</span>
+									</div>
+									<div class="extra-job-info">
+										<a class="apply-thisjob" href="#" title=""><i
+												class="la la-paper-plane"></i>Daftar</a>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="extra-job-info">
-						<span><i class="la la-clock-o"></i><strong>35</strong> Days</span>
-						<span><i class="la la-search-plus"></i><strong>35697</strong> Displayed</span>
-						<span><i class="la la-file-text"></i><strong>300-500</strong> Application</span>
 					</div>
 				</div>
 			</div>
