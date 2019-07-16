9c3946ac98932099bd0f276609954d876f081d70
adistriani <adistriani@gmail.com>
2019-07-16 20:04:26 +0700

    show1

--- resources/views/user/content/post/show.blade.php
+++ resources/views/user/content/post/show.blade.php
@@ -24,7 +24,7 @@
 		<div class="block">
 			<div class="container">
 				 <div class="row">
-				 	<div class="col-lg-9 column">
+				 	<div class="col-lg-12 column">
 				 		<div class="blog-single">
 				 			<div class="bs-thumb"><img src="http://placehold.it/834x340" alt="" /></div>
 				 			<ul class="post-metas"><li><a href="#" title=""><img src="http://placehold.it/40x40" alt="" />Ali TUFAN</a></li><li><a href="#" title=""><i class="la la-calendar-o"></i>November 23, 2017</a></li><li><a class="metascomment" href="#" title=""><i class="la la-comments"></i>4 comments</a></li><li><a href="#" title=""><i class="la la-file-text"></i>Travel, Skill, Jobs</a></li></ul>
@@ -58,76 +58,7 @@
 				 		
 				 		</div>
 					</div>
-					<aside class="col-lg-3 column">
-						<div class="widget">
-				 			<div class="search_widget_job no-margin">
-				 				<div class="field_w_search">
-				 					<input placeholder="Search Keywords" type="text">
-				 					<i class="la la-search"></i>
-				 				</div><!-- Search Widget -->
-				 			</div>
-				 		</div>
-				 		<div class="widget">
-				 			<h3>Categories</h3>
-				 			<div class="sidebar-links">
-				 				<a href="#" title=""><i class="la la-angle-right"></i>Education</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>Information</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>Jobs</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>Learn</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>Skill</a>
-				 			</div>
-				 		</div>
-				 		<div class="widget">
-				 			<h3>Recent Posts</h3>
-				 			<div class="post_widget">
-				 				<div class="mini-blog">
-				 					<span><a href="#" title=""><img src="http://placehold.it/74x64" alt="" /></a></span>
-				 					<div class="mb-info">
-				 						<h3><a href="#" title="">Canada adds 12,500 jobs in modest July rebound</a></h3>
-				 						<span>October 25, 2017</span>
-				 					</div>
-				 				</div>
-				 				<div class="mini-blog">
-				 					<span><a href="#" title=""><img src="http://placehold.it/74x64" alt="" /></a></span>
-				 					<div class="mb-info">
-				 						<h3><a href="#" title="">How to “Woo” a Recruiter and Land Your Dream Job</a></h3>
-				 						<span>October 25, 2017</span>
-				 					</div>
-				 				</div>
-				 				<div class="mini-blog">
-				 					<span><a href="#" title=""><img src="http://placehold.it/74x64" alt="" /></a></span>
-				 					<div class="mb-info">
-				 						<h3><a href="#" title="">Hey Job Seeker, It’s Time To Get Up And Get Hired</a></h3>
-				 						<span>October 25, 2017</span>
-				 					</div>
-				 				</div>
-				 			</div>
-				 		</div>
-				 		<div class="widget">
-				 			<h3>Archives</h3>
-				 			<div class="sidebar-links">
-				 				<a href="#" title=""><i class="la la-angle-right"></i>April 2017</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>March 2016</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>February 2015</a>
-				 				<a href="#" title=""><i class="la la-angle-right"></i>July 2013</a>
-				 			</div>
-				 		</div>
-				 		
-				 		<div class="widget">
-				 			<h3>Our Photo</h3>
-				 			<div class="photo-widget">
-				 				<div class="row">
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 					<div class="col-lg-4 col-md-2 col-sm-2 col-xs-6"> <a href="#" title=""><img src="http://placehold.it/79x57" alt="" /></a> </div>
-				 				</div>
-				 			</div>
-				 		</div>
 				
-					</aside>
 				 </div>
 			</div>
 		</div>
