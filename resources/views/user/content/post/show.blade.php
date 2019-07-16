8d2d2b13e07ac1b9fad5bac435f14dcd2fca6efd
mauritswalalayo <48439158+mauritswalalayo@users.noreply.github.com>
2019-07-16 20:41:48 +0700

    Update show.blade.php

--- resources/views/user/content/post/show.blade.php
+++ resources/views/user/content/post/show.blade.php
@@ -12,7 +12,7 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="inner-header">
-							<h3>Cerita Karier</h3>
+							<h3>{{$post->title}}</h3>
 						</div>
 					</div>
 				</div>
@@ -27,15 +27,11 @@
 				 	<div class="col-lg-12 column">
 				 		<div class="blog-single">
 				 			<div class="bs-thumb"><img src="http://placehold.it/834x340" alt="" /></div>
-				 			<ul class="post-metas"><li><a href="#" title=""><img src="http://placehold.it/40x40" alt="" />Ali TUFAN</a></li><li><a href="#" title=""><i class="la la-calendar-o"></i>November 23, 2017</a></li><li><a class="metascomment" href="#" title=""><i class="la la-comments"></i>4 comments</a></li><li><a href="#" title=""><i class="la la-file-text"></i>Travel, Skill, Jobs</a></li></ul>
-				 			<h2>11 Tips to Help You Get New Clients Through Cold Calling</h2>
-				 			<p>Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much. </p>
-				 			<p>Repeatedly dreamed alas opossum but dramatically despite expeditiously that jeepers loosely yikes that as or eel underneath kept and slept compactly far purred sure abidingly up above fitting to strident wiped set waywardly far the and pangolin horse approving paid chuckled cassowary oh above a much opposite far much hypnotically more therefore wasp less that hey apart well like while superbly orca and far hence one.Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy.</p>
-				 			<h3>About Business Network</h3>
-				 			<p>Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much. </p>
-				 			<blockquote><p><i>“</i><span>You might remember the Dell computer commercials in which a youth reports this exciting news to his friends that youth reports this exciting news to his friends that<i>”</i></span></p><strong>Ali TUFAN</strong></blockquote>
-				 			<p>Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the sullen much connected bat wonderfully on instantaneously eel valiantly petted this along across highhandedly much</p>
-				 			<div class="tags-share">
+				 			<ul class="post-metas"><li><a href="#" title=""><img src="http://placehold.it/40x40" alt="" />Ali TUFAN</a></li><li><a href="#" title=""><i class="la la-calendar-o"></i>{{$post->created_at}}</a></li><li><a class="metascomment" href="#" title=""><i class="la la-comments"></i>4 comments</a></li><li><a href="#" title=""><i class="la la-file-text"></i>Travel, Skill, Jobs</a></li></ul>
+				 			
+                             <h1>{!! $post->content !!}</h1>
+                             
+                             <div class="tags-share">
 						 			<div class="tags_widget">
 						 				<span>Tags</span>
 						 				<a href="#" title="">Adwords</a>
