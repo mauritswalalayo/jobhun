8f7c0a1ec2960016113ad2febb301120f0501cfd
mauritswalalayo <48439158+mauritswalalayo@users.noreply.github.com>
2019-07-23 20:29:37 +0700

    Update formposting.blade.php

--- resources/views/user/content/postingjob/formposting.blade.php
+++ resources/views/user/content/postingjob/formposting.blade.php
@@ -68,21 +68,21 @@
                                          <div class="col-lg-12">
 					 						<span class="pf-title">Deskripsi Perusahaan</span>
 					 						<div class="pf-field">
-					 							<textarea type="text" placeholder="Jobhun merupakan startup pendukung perkembangan karier masyarakat Indonesia. Kami memiliki layanan utama berupa Jobhun Career Hub dan Jobhun Academy. Melalui platform ini, kami akan mempertemukan pencari kerja dan pemberi kerja dengan cara yang cepat dan mudah. Pencari kerja dapat melamar kerja dengan mudah, sedangkan perusahaan dapat menemukan kandidat yang tepat. Sedangkan Jobhun Academy dibuat guna membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent."></textarea>
+					 							<textarea type="text" name="" placeholder="Jobhun merupakan startup pendukung perkembangan karier masyarakat Indonesia. Kami memiliki layanan utama berupa Jobhun Career Hub dan Jobhun Academy. Melalui platform ini, kami akan mempertemukan pencari kerja dan pemberi kerja dengan cara yang cepat dan mudah. Pencari kerja dapat melamar kerja dengan mudah, sedangkan perusahaan dapat menemukan kandidat yang tepat. Sedangkan Jobhun Academy dibuat guna membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent."></textarea>
 					 						</div>
 					 					</div>
                                          
                                         <div class="col-lg-12">
 					 						<span class="pf-title">Alamat Perusahaan</span>
 					 						<div class="pf-field">
-					 							<input type="text" placeholder="Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel, Wonokromo, Surabaya, Jawa Timur, Indonesia 60246" />
+					 							<input type="text" name="office_address" placeholder="Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel, Wonokromo, Surabaya, Jawa Timur, Indonesia 60246" />
 					 						</div>
                                          </div>
 
                                          <div class="col-lg-12">
 					 						<span class="pf-title">Website Perusahaan</span>
 					 						<div class="pf-field">
-					 							<input type="text" placeholder="www.jobhun.id" />
+					 							<input type="text" name="office_website" placeholder="www.jobhun.id" />
 					 						</div>
                                          </div>
 
@@ -96,7 +96,7 @@
                                         <div class="col-lg-12">
 					 						<span class="pf-title">No Telepon Perusahaan</span>
 					 						<div class="pf-field">
-					 							<input type="text" placeholder="08113235533" />
+					 							<input type="text" name="office_phone" placeholder="08113235533" />
 					 						</div>
                                         </div>
                                         
@@ -123,7 +123,7 @@
                                         <div class="col-lg-12">
 					 						<span class="pf-title">Deskripsi Pekerjaan</span>
 					 						<div class="pf-field">
-					 							<textarea type="text" >Syarat dan kualifikasi:
+					 							<textarea type="text" name="description_job">Syarat dan kualifikasi:
 Kemampuan dan kompetensi yang harus dimiliki : 
 Lokasi kerja : 
 Waktu bekerja : 
@@ -132,7 +132,22 @@ Bahasa yang digunakan :
 Tunjangan : 
 Besar gaji : </textarea>
 					 						</div>
-                                        </div>
+										</div>
+
+										<div class="col-lg-12">
+												<span class="pf-title">Deskripsi Pekerjaan</span>
+												<div class="pf-field">
+													<textarea type="text" name="description_job">Syarat dan kualifikasi:
+   Kemampuan dan kompetensi yang harus dimiliki : 
+   Lokasi kerja : 
+   Waktu bekerja : 
+   Gaya berpakaian : 
+   Bahasa yang digunakan : 
+   Tunjangan : 
+   Besar gaji : </textarea>
+												</div>
+										   </div>
+										
                                          
                                         
                                        
