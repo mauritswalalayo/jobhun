04f6bd9064817e6a7500abc278b44e1f819e3a1b
Zunedi <zunedisinambela@gmail.com>
2019-08-09 13:53:11 +0700

    Update

--- resources/views/user/content/postingjob/typejob.blade.php
+++ resources/views/user/content/postingjob/typejob.blade.php
@@ -38,29 +38,49 @@
 									<div class="job-grid border">
 										<div class="job-title-sec">
 											<h3>Membantu menghemat waktu dan biaya</h3>
-											<p align="left">Perusahaan bisa merekrut kandidat terbaik dengan cara yang mudah dan cepat. meminimalkan sumber daya manusia internal serta menghemat biaya untuk melakukan perekrutan</p>
+											<p align="left">Perusahaan bisa merekrut kandidat terbaik dengan cara yang
+												mudah dan cepat. meminimalkan sumber daya manusia internal serta
+												menghemat biaya untuk melakukan perekrutan</p>
 										</div>
 									</div>
 								</div>
 								<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
 									<div class="job-grid border">
 										<div class="job-title-sec">
-											<h3>Berkesempatan untuk mendapatkan pekerjaan</h3>
-											<p align="left">Jobhun bekerjasama dengan berbagai perusahaan dalam mencari
-												kandidat terbaik. Apabila kamu berhasil menyelesaikan program di Jobhun
-												Academy dengan baik dan sesuai dengan Kriteria yang sedang dicari oleh
-												perusahaan, maka Jobhun akan merekomendasikan kamu agar bisa bekerja di
-												beberapa company partner Jobhun tersebut.</p>
+											<h3>Menemukan kandidat terbaik yang potensial</h3>
+											<p align="left">Khusus untuk Jobhun Career Hub Premium, tim rekrutmen
+												profesional Jobhun yang akan melakukan screening terhadap para kandidat.
+												Hanya kandidat potensial terbaik dan sesuai dengan kualifikasi yang akan
+												dipilih. Perusahaan akan menerima database kandidat yang telah siap
+												untuk diwawancara kerja.</p>
 										</div>
 									</div>
 								</div>
 								<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
 									<div class="job-grid border">
 										<div class="job-title-sec">
-											<h3>Dibimbing oleh mentor - mentor berkualitas dan berpengalaman</h3>
-											<p align="left">Jobhun selalu berupaya menghadirkan mentor berkualitas dan
-												berpengalaman dibidangnya, serta memiliki prestasi di bidang tersebut.
-											</p>
+											<h3>Menggunakan <i>channel</i> yang beragam</h3>
+											<p align="left">Perusahaan bisa merekrut kandidat terbaik dengan cara yang
+												mudah dan cepat. meminimalkan sumber daya manusia internal serta
+												menghemat biaya untuk melakukan perekrutan.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+									<div class="job-grid border">
+										<div class="job-title-sec">
+											<h3>Memiliki jaringan yang luas</h3>
+											<p align="left">Jobhun memiliki kerja sama dengan beragam komunitas, organisasi, maupun instansi, sehingga dapat memudahkan pencarian kandidat sesuai dengan kualifikasi yang dibutuhkan.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+									<div class="job-grid border">
+										<div class="job-title-sec">
+											<h3>Memberikan garansi</h3>
+											<p align="left">Ketika kandidat yang dipilih melalui layanan Jobhun Career Hub Premium dirasa belum sesuai, maka Jobhun akan memberikan garansi untuk mencari kandidat lain (dengan perpanjangan waktu), tanpa dipungut biaya. Syarat dan ketentuan berlaku</p>
 										</div>
 									</div>
 								</div>
