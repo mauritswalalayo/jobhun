d1a40260cc2e7d976fb84a7c4dddb4e0b0bba23a
mauritswalalayo <48439158+mauritswalalayo@users.noreply.github.com>
2019-08-10 17:22:35 +0700

    blog

--- resources/views/user/content/mediapartner/formmediapartner.blade.php
+++ resources/views/user/content/mediapartner/formmediapartner.blade.php
@@ -53,11 +53,11 @@
 									
 					 				<div class="row">
                         				<div class="col-lg-12">
-					 						<span class="pf-title">Tipe Posting Media Partner</span>
+					 						<span class="pf-title">Layanan Media Partner</span>
 					 						<div class="pf-field">
 					 							<select data-placeholder="Allow In Search" name="type_loker" class="chosen">
-													<option>Jobhun Career Hub Reguler</option>
-													<option>Jobhun Career Hub Premium</option>
+													<option>Free</option>
+													<option>Premium Rp100.000,-/acara</option>
 												</select>
 					 						</div>
 					 					</div>
@@ -83,104 +83,29 @@
                                                 </div>
                                         </div>
                                          
-                                         <div class="col-lg-12">
-					 						<span class="pf-title">Deskripsi Perusahaan</span>
-					 						<div class="pf-field">
-					 							<textarea type="text" name="description_company" placeholder="Jobhun merupakan startup pendukung perkembangan karier masyarakat Indonesia. Kami memiliki layanan utama berupa Jobhun Career Hub dan Jobhun Academy. Melalui platform ini, kami akan mempertemukan pencari kerja dan pemberi kerja dengan cara yang cepat dan mudah. Pencari kerja dapat melamar kerja dengan mudah, sedangkan perusahaan dapat menemukan kandidat yang tepat. Sedangkan Jobhun Academy dibuat guna membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent."></textarea>
-					 						</div>
-					 					</div>
-                                         
                                         <div class="col-lg-12">
-					 						<span class="pf-title">Alamat Perusahaan</span>
+					 						<span class="pf-title">Tanggal Pelaksanaan Acara</span>
 					 						<div class="pf-field">
 					 							<input type="text" name="company_address" placeholder="Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel, Wonokromo, Surabaya, Jawa Timur, Indonesia 60246" />
 					 						</div>
                                          </div>
 
                                          <div class="col-lg-12">
-					 						<span class="pf-title">Website Perusahaan</span>
+					 						<span class="pf-title">Tempat Pelaksanaan Acara</span>
 					 						<div class="pf-field">
 					 							<input type="text" name="company_website" placeholder="www.jobhun.id" />
 					 						</div>
                                          </div>
 
                                          <div class="col-lg-12">
-					 						<span class="pf-title">Email Perusahaan</span>
-					 						<div class="pf-field">
-					 							<input type="text" name="company_email" placeholder="jobhun@jobhun.id" />
-					 						</div>
-                                         </div>
-
-                                        <div class="col-lg-12">
-					 						<span class="pf-title">No Telepon Perusahaan</span>
-					 						<div class="pf-field">
-					 							<input type="text" name="company_phone" placeholder="08113235533" />
-					 						</div>
-                                        </div>
-                                        
-                                        <div class="col-lg-12">
-					 						<span class="pf-title">Posisi yang dicari</span>
-					 						<div class="pf-field">
-					 							<input type="text" name="position_sought" placeholder="Programmer" />
-					 						</div>
+                                                <span class="pf-title">Detail Acara</span>
+                                                <div class="pf-field">
+                                                    <textarea type="text" name="description_company" placeholder="Jobhun merupakan startup pendukung perkembangan karier masyarakat Indonesia. Kami memiliki layanan utama berupa Jobhun Career Hub dan Jobhun Academy. Melalui platform ini, kami akan mempertemukan pencari kerja dan pemberi kerja dengan cara yang cepat dan mudah. Pencari kerja dapat melamar kerja dengan mudah, sedangkan perusahaan dapat menemukan kandidat yang tepat. Sedangkan Jobhun Academy dibuat guna membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent."></textarea>
+                                                </div>
                                         </div>
                                     
-					 					<div class="col-lg-12">
-					 						<span class="pf-title">Jenis Pekerjaan</span>
-					 						<div class="pf-field">
-					 							<select data-placeholder="Allow In Search" name="type_work" class="chosen">
-													<option>Full time</option>
-                                                    <option>Part time</option>
-                                                    <option>Freelance</option>
-                                                    <option>Internship</option>
-                                                    <option>Volunteer</option>
-												</select>
-					 						</div>
-                                         </div>
-
-                                        <div class="col-lg-12">
-					 						<span class="pf-title">Deskripsi masing-masing Pekerjaan</span>
-					 						<div class="pf-field">
-					 							<textarea type="text" name="description_job">Syarat dan kualifikasi :
-Kemampuan dan kompetensi yang harus dimiliki : 
-Lokasi kerja : 
-Waktu bekerja : 
-Gaya berpakaian : 
-Bahasa yang digunakan : 
-Tunjangan : 
-Besar gaji : </textarea>
-					 						</div>
-										</div>
-
-										<div class="col-lg-12">
-												<span class="pf-title">Tentang Proses Rekrut</span>
-												<div class="pf-field">
-													<textarea type="text" name="recruit_process">
-Cara mengirimkan lamaran :
-Batas waktu melamar :
-Waktu proses rekrut.
-													</textarea>
-												</div>
-										   </div>
-										
-                                         
+					 					
                                         
-										   <div class="col-lg-12">
-												<span class="pf-title">Unggah Logo Perusahaan</span>
-												<div class="upload-info">
-												   <input type="file" name="logo_url" id="logo_url" class="form-control-file" >
-												   <span>Klik tombol 'Upload', kemudian pilih logo perusahaan yang ingin kamu unggah.</span>
-											   </div>
-										   </div>
-
-                                        <div class="col-lg-12">
-                                             <span class="pf-title">Unggah Poster</span>
-                                             <div class="upload-info">
-                                                <input type="file" class="form-control-file" name="upload_poster">
-                                                <span>Klik tombol 'Upload', kemudian pilih poster yang ingin kamu unggah.</span>
-                                            </div>
-                                        </div>
-
                                         <div class="col-lg-12">
                                              <span class="pf-title">Bukti Transfer</span>
                                              <div class="upload-info">
