a524bd600786762c00ac0aadcff8e83e818a4650
Zunedi Suprianto Sinambela <42605944+Zunedi@users.noreply.github.com>
2019-10-30 19:57:26 +0700

    s

--- public/js/app.js
+++ public/js/app.js
@@ -93,7 +93,7 @@
 /*! no exports provided */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: EPERM: operation not permitted, open 'C:\\xampp\\htdocs\\Github\\jobhun\\resources\\js\\app.js'");
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'C:\\xampp\\htdocs\\Github\\jobhun\\resources\\js\\app.js'");
 
 /***/ }),
 
