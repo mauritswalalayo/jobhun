ab49d4873884829a87eb2f78d3d36958cfa98800
Zunedi Suprianto Sinambela <42605944+Zunedi@users.noreply.github.com>
2019-11-01 18:58:39 +0700

    Update Back Vue

---
+++ resources/js/components/back/tag/Table.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <a :href="$store.state.apiUrl + 'tag/form'" class="btn btn-primary">Tambah Tag</a>
+            </div>
+            <div class="box-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name Tag</th>
+                            <th>Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <a href=""
+                                    class="btn btn-warning btn-sm glyphicon glyphicon-pencil">Edit</a>
+                                <a href=""
+                                    class="btn btn-danger btn-sm glyphicon glyphicon-trash">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="box-footer">
+                Footer
+            </div>
+        </div>
+    </div>
+</template>
