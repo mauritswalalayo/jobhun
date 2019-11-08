288a01aabe96798a9b1693d7146bb623c6911734
yunusWidodo <younews372@gmail.com>
2019-11-08 19:06:59 +0700

    coba branch yunus

---
+++ resources/views/yunus.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>yunusbrance</title>
+</head>
+<body>
+    <h1>yunus brance</h1>
+    
+</body>
+</html>
