34fadd0eda98b274f94fc79fd57f824b536f8ec0
yunusWidodo <younews372@gmail.com>
2019-11-11 18:52:59 +0700

    lupa

---
+++ resources/js/components/mobile/content/carrerHubDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class=" container">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <i class="fas fa-credit-card"></i>
+            </li>
+            <li class="nav-item">
+                <i class="fas fa-map-marked-alt"></i>
+            </li>
+            <li class="nav-item">
+                <i class="fas fa-calendar-check"></i>
+            </li>
+         </ul>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
