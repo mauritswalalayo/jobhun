519f26b1c62f4594ccc5640c8bd6da4339455fb1
Zunedi Suprianto Sinambela <42605944+Zunedi@users.noreply.github.com>
2019-11-11 18:58:27 +0700

    Update

--- resources/js/components/back/testimoni/Form.vue
+++ resources/js/components/back/testimoni/Form.vue
@@ -1,45 +1,43 @@
 <template>
-<div>
-            <div>
-
+    <div>
+        <div>
             <form action="">
                 <!-- <form action="" method="POST" enctype="multipart/form-data"> -->
-
-                    <input value="">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Nama :</label>
-                            <input name="name" type="text" id="name" class="form-control"
-                                placeholder="Masukan Nama Anda......">
-                        </div>
+                <input value="">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Nama :</label>
+                        <input name="name" type="text" id="name" class="form-control"
+                            placeholder="Masukan Nama Anda......">
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Profesi :</label>
-                            <input name="profesi" type="text" id="profesi"
-                                class="form-control" placeholder="Masukan Profesi Anda......">
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Profesi :</label>
+                        <input name="profesi" type="text" id="profesi" class="form-control"
+                            placeholder="Masukan Profesi Anda......">
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Testimoni :</label>
-                            <textarea name="testimoni" id="testimoni" cols="30" rows="10" class="form-control">  </textarea>
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Testimoni :</label>
+                        <textarea name="testimoni" id="testimoni" cols="30" rows="10" class="form-control">  </textarea>
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Foto :</label>
-                            <input type="file" name="foto" id="foto" cols="30" rows="10" class="form-control">
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Foto :</label>
+                        <input type="file" name="foto" id="foto" cols="30" rows="10" class="form-control">
                     </div>
+                </div>
 
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-info pull-right">Update</button>
-                    </div>
-                </form>
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-info pull-right">Update</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
