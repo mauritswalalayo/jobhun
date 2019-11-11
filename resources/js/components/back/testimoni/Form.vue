25bc90df00ce246ebf84f5d6b28b08db9801f95e
Zunedi Suprianto Sinambela <42605944+Zunedi@users.noreply.github.com>
2019-11-11 18:14:18 +0700

    Update Form Testimoni

--- resources/js/components/back/testimoni/Form.vue
+++ resources/js/components/back/testimoni/Form.vue
@@ -2,56 +2,54 @@
     <div>
         <div class="box box-primary">
             <div class="box-header with-border">
-                <a href="" class="btn btn-danger">Kembali</a>
+                <a :href="$store.state.apiUrl + 'testimoni/table'" class="btn btn-danger">Kembali</a>
             </div>
+
             <div class="box-body">
                 <div class="alert alert-danger">
                     <ul>
                         <li></li>
                     </ul>
                 </div>
-
             </div>
 
-            <form action="" method="PATCH">
-                <input type="hidden" name="_method" value="PATCH">
-
-                <form action="" method="POST" enctype="multipart/form-data">
-
-                    <input type="hidden" name="_token" value="">
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Nama :</label>
-                            <input name="name" type="text" id="name" class="form-control" placeholder="Masukan Nama Anda......">
-                        </div>
+            <form action="">
+                <input>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Nama :</label>
+                        <input name="name" type="text" id="name" class="form-control"
+                            placeholder="Masukan Nama Anda......">
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Profesi :</label>
-                            <input name="profesi" type="text" id="profesi"
-                                class="form-control" placeholder="Masukan Profesi Anda......">
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Profesi :</label>
+                        <input name="profesi" type="text" id="profesi" class="form-control"
+                            placeholder="Masukan Profesi Anda......">
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Testimoni :</label>
-                            <textarea name="testimoni" id="testimoni" cols="30" rows="10" class="form-control">  </textarea>
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Testimoni :</label>
+                        <textarea name="testimoni" id="testimoni" cols="30" rows="10" class="form-control">  </textarea>
                     </div>
+                </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <label for="title">Foto :</label>
-                            <input type="file" name="foto" id="foto" cols="30" rows="10" class="form-control">
-                        </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="title">Foto :</label>
+                        <input type="file" name="foto" id="foto" cols="30" rows="10" class="form-control">
                     </div>
+                </div>
 
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-info pull-right">Update</button>
-                    </div>
-                </form>
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-info pull-right">Update</button>
+                    <button type="submit" class="btn btn-info pull-right">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
